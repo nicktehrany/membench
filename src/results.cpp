13c4a23f9af88c2b69c66ad0226305044a693327
Nick <nicktehrany1@gmail.com>
2020-04-24 20:26:50 +0200

    Improved output Formatting

--- src/results.cpp
+++ src/results.cpp
@@ -28,31 +28,58 @@ void dump_results(Results results, Arguments args)
     switch (args.map_anon)
     {
     case 1:
-        outfile << "Map_ANONYMOUS\t\t\tYes\n";
+        outfile << "MAP_ANONYMOUS\t\t\tYes\n";
         break;
     default:
-        outfile << "Map_ANONYMOUS\t\t\tNo\n";
+        outfile << "MAP_ANONYMOUS\t\t\tNo\n";
         break;
     }
+    if (args.fsize > (1024 * 1024 * 1024))
+        outfile << "File Size\t\t\t\t" << args.fsize / (1024 * 1024 * 1024) << " GiB\n";
+    else if (args.fsize > (1024 * 1024))
+        outfile << "File Size\t\t\t\t" << args.fsize / (1024 * 1024) << " MiB\n";
+    else if (args.fsize > 1024)
+        outfile << "File Size\t\t\t\t" << args.fsize / 1024 << " KiB\n";
+    else
+        outfile << "File Size\t\t\t\t" << args.fsize << " Bytes\n";
 
-    outfile << "File Size\t\t\t\t" << args.fsize << " Bytes\n";
-    outfile << "Block Size\t\t\t\t" << args.buflen << " Bytes\n";
+    if (args.buflen > (1024 * 1024 * 1024))
+        outfile << "Block Size\t\t\t\t" << args.buflen / ((1024 * 1024 * 1024)) << " GiB\n";
+    else if (args.buflen > (1024 * 1024))
+        outfile << "Block Size\t\t\t\t" << args.buflen / ((1024 * 1024)) << " MiB\n";
+    else if (args.buflen > 1024)
+        outfile << "Block Size\t\t\t\t" << args.buflen / 1024 << " KiB\n";
+    else
+        outfile << "Block Size\t\t\t\t" << args.buflen << " Bytes\n";
 
     switch (args.mode)
     {
     case 0:
-        outfile << "Sequential Read\t\t\t" << results.bandwidth << " MiB/s\n";
+        if (results.bandwidth > 1024)
+            outfile << "Sequential Read\t\t\t" << results.bandwidth / 1024 << " GiB/s\n";
+        else
+            outfile << "Sequential Read\t\t\t" << results.bandwidth << " MiB/s\n";
         break;
     case 1:
-        outfile << "Sequential Write\t\t" << results.bandwidth << " MiB/s\n";
+        if (results.bandwidth > 1024)
+            outfile << "Sequential Write\t\t\t" << results.bandwidth / 1024 << " GiB/s\n";
+        else
+            outfile << "Sequential Write\t\t\t" << results.bandwidth << " MiB/s\n";
         break;
     case 2:
-        outfile << "Random Read\t\t\t\t" << results.bandwidth << " MiB/s\n";
+        if (results.bandwidth > 1024)
+            outfile << "Random Read\t\t\t" << results.bandwidth / 1024 << " GiB/s\n";
+        else
+            outfile << "Random Read\t\t\t" << results.bandwidth << " MiB/s\n";
         break;
     case 3:
-        outfile << "Random Write\t\t\t" << results.bandwidth << " MiB/s\n";
+        if (results.bandwidth > 1024)
+            outfile << "Random Write\t\t\t" << results.bandwidth / 1024 << " GiB/s\n";
+        else
+            outfile << "Random Write\t\t\t" << results.bandwidth << " MiB/s\n";
         break;
     }
+
     outfile << "I/O Data\t\t\t\t" << results.io_data << " GiB\n";
 
     outfile.close();
