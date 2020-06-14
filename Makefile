7cf688fdf853aacfeed0da647127ab6d0cc78480
Nick <nicktehrany1@gmail.com>
2020-06-14 18:17:18 +0200

    Updated logger for timestamps

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@ CC = gcc
 CFLAGS = -O2 -Wall -Wextra -g -pedantic-errors
 FULL_DEPS = src/main.c src/results.c engines/mmap.c \
 engines/mmap_lat.c engines/mem_lat.c engines/page_fault.c \
-src/parser.c src/functions.c
+engines/pmem_cline.c src/parser.c src/functions.c
 FULL_DEPS_H = src/results.h engines/mmap.h engines/mmap_lat.h \
-engines/mem_lat.h engines/page_fault.h src/parser.h src/functions.h
+engines/mem_lat.h engines/page_fault.h engines/pmem_cline.h \
+src/parser.h src/functions.h
 BASIC_DEPS = src/main.c src/results.c engines/mmap.c \
 engines/mmap_lat.c engines/mem_lat.c engines/page_fault.c \
 src/parser.c src/functions.c
@@ -20,7 +21,7 @@ all: full
 
 full: $(FULL_DEPS) $(FULL_DEPS_H)
 	@echo "Compiling Full Verison"
-	$(CC) $(CFLAGS) -DPMEM $^ -o $(BM_Target)
+	$(CC) $(CFLAGS) -DPMEM $^ -lpmem -o $(BM_Target)
 
 basic: $(BASIC_DEPS) $(BASIC_DEPS_H)
 	@echo "Compiling Basic Version without PMEM"
--- src/logger.h
+++ src/logger.h
@@ -1,10 +1,13 @@
 #ifndef LOGGER_H
 #include <errno.h>
+#include <time.h>
+#include <stdio.h>
 // LOG ERROR DEBUG or FATAL as type, errno value as error_val (0 for DEBUG and ERROR), 
 // and error Message as message
+static inline char *gettime();
 #define LOG(type, error_val, message) \
-    (type == DEBUG) ? (void)printf("DEBUG: %s\n", message) : ((type == FATAL) ? \
-    (errno = error_val, perror(message), exit(1)) : (void)printf("Error: %s\n", message))
+    (type == DEBUG) ? (void)printf("DEBUG: %s %s\n", gettime(), message) : ((type == FATAL) ? \
+    (errno = error_val, perror(message), exit(1)) : (void)printf("Error: %s %s\n", __TIME__, message))
 
 enum LOG_TYPES
 {
@@ -12,5 +15,18 @@ enum LOG_TYPES
     ERROR = 2,
     DEBUG = 4
 };
+
+static inline char *gettime() 
+{
+    static char buf[64];
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+    snprintf(buf, sizeof(buf), "%d-%d-%d %d:%d:%d", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday, \
+     timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+    return buf;
+}
 
 #endif
